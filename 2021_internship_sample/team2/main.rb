23b12f1ac7f803b222e97f942d702a7800572aab
s205010 <s205010@matsu.shimane-u.ac.jp>
2021-08-30 15:27:27 +0900

    100×100の位置にプレイヤーを表示する

--- 2021_internship_sample/team2/main.rb
+++ 2021_internship_sample/team2/main.rb
@@ -5,9 +5,11 @@ Window.width = 700
 Window.height = 800
 Window.caption = "team2"
 
+image = Image.load("images/player.png")
+image.set_color_key(C_WHITE)
+
 Window.loop do
     break if Input.key_push?(K_ESCAPE)
-
-
+    Window.draw(100, 100, image)
 
   end
