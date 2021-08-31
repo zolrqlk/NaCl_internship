1020e1d97d248cc66f625c75224c1fbe1925e716
s205010 <s205010@matsu.shimane-u.ac.jp>
2021-08-31 11:33:21 +0900

    プレイヤーの移動制限

--- team2/scenes/game/player.rb
+++ team2/scenes/game/player.rb
@@ -9,6 +9,20 @@ module Game
             self.x += Input.x * @speed
             self.y += Input.y * @speed
 
+            /プレイヤーがウィンドウからはみ出さないようにする/
+            if self.x <= 0
+                self.x = 0
+            elsif self.x + 64 >= 700
+                self.x = 636
+            end
+            if self.y <= 0
+                self.y = 0
+            elsif self.y + 64 >= 800
+                self.y = 736
+            end
+            
+        end
+
         def hit(obj)
             
         end
