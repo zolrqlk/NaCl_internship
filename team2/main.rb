ea2c2a971d3e2e127d4ae7e596505e57bd4dc714
esumi <esutb33@gmail.com>
2021-08-31 10:11:11 +0900

    雛形の追加

--- team2/main.rb
+++ team2/main.rb
@@ -1,15 +1,20 @@
 require 'dxruby'
 
+require_relative 'scene'
+require_relative 'scenes/game/director'
+require_relative 'scenes/game/enemy'
+require_relative 'scenes/game/player'
+require_relative 'scenes/game/score'
 
 Window.width = 700
 Window.height = 800
 Window.caption = "team2"
 
-image = Image.load("images/player.png")
-image.set_color_key(C_WHITE)
+Scene.add(Game::Director.new, :game)
+Scene.move_to(:game)
 
 Window.loop do
     break if Input.key_push?(K_ESCAPE)
-    Window.draw(100, 100, image)
+    Scene.play
 
   end
