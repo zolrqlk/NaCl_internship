dd5971c39de6becbfa47d50f26a46cfb784da8fb
d-narai <shikadisk@outlook.jp>
2021-09-03 11:22:21 +0900

    完成！

--- team2/scenes/game/sounds.rb
+++ team2/scenes/game/sounds.rb
@@ -1,15 +1,21 @@
 class Sounds
     def initialize
-        @@shot_effect = Sound.new("sounds/shot_effect_01_edited.wav")
-        @@bgm = Sound.new("sounds/bgm_01.mid")
+        @@shot_effect = Sound.new("sounds/shot_01.wav")
+        @@hit_effect = Sound.new("sounds/hit_01.wav")
+        @@bgm = Sound.new("sounds/game.mid")
     end
 
     def bgm
+        @@bgm.setVolume(215)
         @@bgm.play
     end
 
     def self.shot_effect
         @@shot_effect.play
+    end
+
+    def self.hit_effect
+        @@hit_effect.play
     end
 
     def self.init
--- team2/scenes/opening/sounds.rb
+++ team2/scenes/opening/sounds.rb
@@ -1,18 +1,28 @@
 class Sounds
-    def initialize
-        @@shot_effect = Sound.new("sounds/shot_effect_01_edited.wav")
-        @@bgm = Sound.new("sounds/bgm_01.mid")
+    @@shot_effect = Sound.new("sounds/shot_01.wav")
+    @@hit_effect = Sound.new("sounds/hit_01.wav")
+    @@bgm_game = Sound.new("sounds/game.mid")
+    @@bgm_op = Sound.new("sounds/opening.mid")
+
+    def self.bgm_game
+        @@bgm_game.setVolume(215)
+        @@bgm_game.play
     end
 
-    def self.bgm
-        @@bgm.play
+    def self.bgm_op
+        @@bgm_op.play
     end
 
     def self.shot_effect
         @@shot_effect.play
     end
 
+    def self.hit_effect
+        @@hit_effect.play
+    end
+
     def self.init
-        @@bgm.stop
+        @@bgm_game.stop
+        @@bgm_op.stop
     end
 end
